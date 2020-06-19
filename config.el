1dfbeb820e4bce9088fa7cec21ffb922575ead3c
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-06-19 12:28:04 -0700

    Fix cider repl mode company map

--- config.el
+++ config.el
@@ -120,7 +120,61 @@
          (:map (clojure-mode-map clojurescript-mode-map)
           "," #'cider))))
 
-(add-hook 'cider-repl-mode-hook #'evil-normalize-keymaps)
+(after! cider
+  (add-hook 'company-completion-started-hook 'custom/set-company-maps)
+  (add-hook 'company-completion-finished-hook 'custom/unset-company-maps)
+  (add-hook 'company-completion-cancelled-hook 'custom/unset-company-maps))
+
+(defun custom/unset-company-maps (&rest unused)
+  "Set default mappings (outside of company).
+    Arguments (UNUSED) are ignored."
+  (general-def
+    :states 'insert
+    :keymaps 'override
+    "<down>" nil
+    "<up>"   nil
+    "RET"    nil
+    [return] nil
+    "C-n"    nil
+    "C-p"    nil
+    "C-j"    nil
+    "C-k"    nil
+    "C-h"    nil
+    "C-u"    nil
+    "C-d"    nil
+    "C-s"    nil
+    "C-S-s"   (cond ((featurep! :completion helm) nil)
+                    ((featurep! :completion ivy)  nil))
+    "C-SPC"   nil
+    "TAB"     nil
+    [tab]     nil
+    [backtab] nil))
+
+(defun custom/set-company-maps (&rest unused)
+  "Set maps for when you're inside company completion.
+    Arguments (UNUSED) are ignored."
+  (general-def
+    :states 'insert
+    :keymaps 'override
+    "<down>" #'company-select-next
+    "<up>" #'company-select-previous
+    "RET" #'company-complete
+    [return] #'company-complete
+    "C-w"     nil  ; don't interfere with `evil-delete-backward-word'
+    "C-n"     #'company-select-next
+    "C-p"     #'company-select-previous
+    "C-j"     #'company-select-next
+    "C-k"     #'company-select-previous
+    "C-h"     #'company-show-doc-buffer
+    "C-u"     #'company-previous-page
+    "C-d"     #'company-next-page
+    "C-s"     #'company-filter-candidates
+    "C-S-s"   (cond ((featurep! :completion helm) #'helm-company)
+                    ((featurep! :completion ivy)  #'counsel-company))
+    "C-SPC"   #'company-complete-common
+    "TAB"     #'company-complete-common-or-cycle
+    [tab]     #'company-complete-common-or-cycle
+    [backtab] #'company-select-previous    ))
 
 (map! :leader
       :desc "Last buffer" "TAB" #'mode-line-other-buffer)
